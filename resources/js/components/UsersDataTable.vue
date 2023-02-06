6d47a3b8adf5108eca5814738a6a4d16bea3f7c6
Alexander Shutov <san4o528@gmail.com>
2023-02-06 19:30:52 +0400

    feat(Vue): fixed user address display; removed default library heading

--- resources/js/components/UsersDataTable.vue
+++ resources/js/components/UsersDataTable.vue
@@ -27,7 +27,7 @@
             data: 'userAddress',
             orderable: false,
             render: function(data) {
-                if (!data || !data.hasOwnProperty('street'))
+                if (!data)
                     return '';
                 return [data.street, data.city,data.country, data.zip].join(', ');
             }
@@ -40,16 +40,7 @@
 
 <template>
 <div class="container">
-    <h1>Ajax data source table</h1>
-    <h2>DataTables + Vue3 example</h2>
-    <p>
-        In this example the <code>datatables.net-vue3</code> package is used to
-        load JSON data and display it in an interactive table. In this case we use
-        object based row data, rather than arrays, so the
-        <code>columns</code> option is specified to tell DataTables which property
-        to read from the objects for each column.
-    </p>
-
+    <h1>Bloomex Users</h1>
     <DataTable
             :columns="columns"
             ajax="/api/users"
